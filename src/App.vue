2180237b48d36e13936419a189081fbe254955ef
prh <1875377706@qq.com>
2022-06-23 10:30:40 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,7 @@ useHead({
       name: 'theme-color',
       content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
     },
-  ],
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/svg+xml',
-      href: computed(() => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg'),
-    },
-  ],
+  ]
 })
 </script>
 

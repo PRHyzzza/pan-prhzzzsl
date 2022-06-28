2ec9d1c220f1539cff7e3be6322c88395117d045
prh <1875377706@qq.com>
2022-06-28 15:49:59 +0800

    1

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -1,20 +1,5 @@
-<script setup lang="ts">
-const { height, width } = useWindowSize()
-
-const box = reactive({
-  width: width.value + 'px',
-  height: height.value + 'px',
-})
-watch(height, () => {
-  box.height = height.value + 'px'
-})
-watch(width, () => {
-  box.width = width.value + 'px'
-})
-</script>
-
 <template>
-  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200" :style="box">
+  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200" h-full w-full>
     <RouterView />
     <Footer />
     <Record />

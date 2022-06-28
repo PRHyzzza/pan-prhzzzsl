3a3ba7de1404c4ea38fd6af996eb6ab2d3c74237
prh <1875377706@qq.com>
2022-06-28 16:04:15 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -91,26 +91,15 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
       manifest: {
-        name: 'Vitesse',
-        short_name: 'Vitesse',
+        name: 'pan',
+        short_name: 'pan',
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/pan.png',
             sizes: '192x192',
             type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
+          }
         ],
       },
     }),
